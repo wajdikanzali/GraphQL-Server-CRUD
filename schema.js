902c32f296f779acb97fb835807e4cf13a780e2c
Wajdi Kanzali <kanzali.wajdi@gmail.com>
2018-01-02 00:00:23 +0100

    Json Server & Axios

--- schema.js
+++ schema.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const {
     GraphQLObjectType,
     GraphQLString,
@@ -8,11 +10,11 @@ const {
 } = require('graphql');
 
 // Hardcoded data
-const customers = [
+/*const customers = [
     {id:'1', name:'John', email:'Jhon@gmail.com', age:35},
     {id:'2', name:'Steve', email:'steve@gmail.com', age:25},
     {id:'3', name:'Alex', email:'alex@gmail.com', age:18}
-];
+];*/
 
 
 // Customer Type
@@ -36,17 +38,20 @@ const RootQuery= new GraphQLObjectType({
                 id:{type:GraphQLString}
             },
             resolve(parentValue, args){
-                for(let i=0;i < customers.length;i++){
+                /* for(let i=0;i < customers.length;i++){
                     if(customers[i].id == args.id){
                         return customers[i];
                     }
-                } 
+                } */
+                return axios.get('http://localhost:3000/customers/'+args.id)
+                    .then(res => res.data);
             }
         },
         customers:{
             type: new GraphQLList(CustomerType),
             resolve(parentValue, args){
-                return customers;
+                return axios.get('http://localhost:3000/customers')
+                .then(res => res.data);
             }
         }
     }  
